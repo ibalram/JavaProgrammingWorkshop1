44f9b71b3e21e27923f4318b5e3eebe838056642
ibalram <balramsinghrathore3@gmail.com>
2020-10-07 09:14:11 +0530

    Added: Create a tic tac toe board

--- src/com/workshop/TicTacToeGame.java
+++ src/com/workshop/TicTacToeGame.java
@@ -1,10 +1,19 @@
 package com.workshop;
 
+import java.util.Arrays;
+
 public class TicTacToeGame {
+	private char[] board = new char[10];
+	
+	public char[] createBoard() {
+		char[] board = new char[10];
+		Arrays.fill(board,' ');
+		return board;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TicTacToeGame game = new TicTacToeGame();
+		game.createBoard();
 	}
 
 }
