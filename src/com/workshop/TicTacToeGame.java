c469e6fbd44f039ad4e5970f5174b1438694e67d
ibalram <balramsinghrathore3@gmail.com>
2020-10-10 02:34:37 +0530

    Added: Computer play in turn and check for winning position before playing

--- src/com/workshop/TicTacToeGame.java
+++ src/com/workshop/TicTacToeGame.java
@@ -22,6 +22,29 @@ public class TicTacToeGame {
 		System.out.println("Created empty game board");
 	}
 
+	public void showBoard() {
+		for (int i = 1; i < 10; ++i) {
+			System.out.print(String.format("[%s]", board[i]));
+			System.out.print((i % 3 == 0) ? "\n" : "");
+		}
+	}
+
+	public void playFirst() {
+		System.out.println("Enter choice (Head or Tail): ");
+		String userChoice = sc.nextLine();
+		Random rand = new Random();
+		String toss = (rand.nextInt(2) + 1 == 1) ? "Head" : "Tail";
+		if (toss.equalsIgnoreCase(userChoice)) {
+			System.out.println("User goes first");
+			user = 1;
+			selectUserSign();
+		} else {
+			System.out.println("Computer goes first");
+			user = 0;
+			selectComputerSign();
+		}
+	}
+
 	public void selectUserSign() {
 		System.out.println("Enter user choice to select mark/sign (X or O):");
 		userSign = sc.nextLine().toUpperCase().charAt(0);
@@ -31,30 +54,22 @@ public class TicTacToeGame {
 			computerSign = 'X';
 		System.out.println(String.format("User player sign: %s, Computer player sign: %s", userSign, computerSign));
 	}
-	
+
 	public void selectComputerSign() {
-		char computerSign;
 		Random rand = new Random();
-		computerSign = (rand.nextInt(2)+1==1)?'X':'O';
+		computerSign = (rand.nextInt(2) + 1 == 1) ? 'X' : 'O';
 		if (computerSign == 'X')
 			userSign = 'O';
 		else
 			userSign = 'X';
 		System.out.println(String.format("User player sign: %s, Computer player sign: %s", userSign, computerSign));
 	}
-	
-	public void showBoard() {
-		for (int i = 1; i < 10; ++i) {
-			System.out.print(String.format("[%s]", board[i]));
-			System.out.print((i % 3 == 0) ? "\n" : "");
-		}
-	}
 
 	public void userMove() {
 		System.out.println("Enter a position to move(1 to 9): ");
 		int move = Integer.parseInt(sc.nextLine());
 		if (move < 0 || move > 9 || board[move] != ' ') {
-			System.out.println(((board[move] != ' ')?"Already Occupied. ":"")+"Enter a valid move: ");
+			System.out.println(((board[move] != ' ') ? "Already Occupied. " : "") + "Enter a valid move: ");
 			userMove();
 		} else {
 			System.out.println("The position is free to move:");
@@ -63,57 +78,91 @@ public class TicTacToeGame {
 		}
 	}
 
-	public void playFirst() {
-		System.out.println("Enter choice (Head or Tail): ");
-		String userChoice = sc.nextLine();
-		Random rand = new Random();
-		String toss = (rand.nextInt(2)+1==1)?"Head":"Tail";
-		if(toss.equalsIgnoreCase(userChoice)){
-			System.out.println("User goes first");
-			user = 1;
-			selectUserSign();
+	public void computerMove() {
+		int move = positionToWin(computerSign);
+		if (move == -1) {
+			Random rand = new Random();
+			move = rand.nextInt(9) + 1;
 		}
-		else {
-			System.out.println("Computer goes first");
-			user = 0;
-			selectComputerSign();
+		if (board[move] != ' ') {
+			computerMove();
+		} else {
+			System.out.println("Computer makes move");
+			board[move] = computerSign;
+			showBoard();
 		}
 	}
-	
-	public void changeTurn() {
-		user^=1;
+
+	public int positionToWin(char player) {
+		int countLeftDiag = 0, posLeftDiag = -1;
+		int countRightDiag = 0, posRightDiag = -1;
+		for (int i = 0; i < 3; ++i) {
+			int countHorizontal = 0, posHr = -1;
+			int countVertical = 0, posVr = -1;
+
+			countLeftDiag += board[i * 3 + i + 1] == player ? 1 : 0;
+			posLeftDiag = board[i * 3 + i + 1] == ' ' ? (i * 3 + i + 1) : posLeftDiag;
+			countRightDiag += board[i * 3 + (2 - i) + 1] == player ? 1 : 0;
+			posRightDiag = board[i * 3 + (2 - i) + 1] == ' ' ? (i * 3 + (2 - i) + 1) : posRightDiag;
+			for (int j = 0; j < 3; ++j) {
+				countHorizontal += (board[i * 3 + j + 1] == player) ? 1 : 0;
+				posHr = board[i * 3 + j + 1] == ' ' ? (i * 3 + j + 1) : posHr;
+				countVertical += (board[j * 3 + i + 1] == player) ? 1 : 0;
+				posVr = board[j * 3 + i + 1] == ' ' ? (j * 3 + i + 1) : posVr;
+			}
+			if (countHorizontal == 2 && posHr != -1)
+				return posHr;
+			if (countVertical == 2 && posVr != -1)
+				return posVr;
+		}
+		if (countLeftDiag == 2 && posLeftDiag != -1)
+			return posLeftDiag;
+		if (countRightDiag == 2 && posRightDiag != -1) {
+			return posRightDiag;
+		}
+		return -1;
 	}
-	
-	public boolean isUserTurn() {
-		return user==1;
-	}
-	
+
 	public boolean checkWinner(char player) {
 		boolean win = false;
 		boolean matchLeftDiag = true;
 		boolean matchRightDiag = true;
-		for(int i = 0; i<3; ++i) {
+		for (int i = 0; i < 3; ++i) {
 			boolean matchHorizontal = true;
 			boolean matchVertical = true;
-			
-			matchLeftDiag &= board[i*3+i+1]==player;
-			matchRightDiag &= board[i*3 +(2-i)+1]==player;
-			for (int j = 0; j<3; ++j) {
-				matchHorizontal &= board[i*3+j+1]==player;
-				matchVertical &= board[j*3+i+1]==player;
+
+			matchLeftDiag &= board[i * 3 + i + 1] == player;
+			matchRightDiag &= board[i * 3 + (2 - i) + 1] == player;
+			for (int j = 0; j < 3; ++j) {
+				matchHorizontal &= board[i * 3 + j + 1] == player;
+				matchVertical &= board[j * 3 + i + 1] == player;
 			}
 			if (matchHorizontal || matchVertical) {
 				win = true;
 				break;
 			}
 		}
-		if (matchLeftDiag || matchRightDiag)
-			win = true;
-		if (win) {
-			System.out.println(((player==userSign)?"User":"Computer")+"Won!!");
+		if (win || matchLeftDiag || matchRightDiag) {
+			System.out.println(((player == userSign) ? "User" : "Computer") + " Won!!");
 			return true;
 		}
 		return false;
+	}
+
+	public boolean tie() {
+		int count = 0;
+		for (int i = 1; i <= 9; ++i) {
+			count += board[i] != ' ' ? 1 : 0;
+		}
+		return count == 9;
+	}
+
+	public void changeTurn() {
+		user ^= 1;
+	}
+
+	public boolean isUserTurn() {
+		return user == 1;
 	}
 
 	public void setUserSign(char sign) {
@@ -136,24 +185,24 @@ public class TicTacToeGame {
 		Scanner sc = new Scanner(System.in);
 		TicTacToeGame game = new TicTacToeGame(sc);
 		game.createBoard();
-		//game.selectUserSign();
+		// game.selectUserSign();
 		game.playFirst();
 		game.showBoard();
-		game.userMove();
-		game.userMove();
 		while (true) {
 			if (game.isUserTurn()) {
 				game.userMove();
 				if (game.checkWinner(game.getUserSign())) {
 					break;
 				}
+			} else {
+				game.computerMove();
+				if (game.checkWinner(game.getComputerSign())) {
+					break;
+				}
 			}
-			else {
-				continue;
-				//game.computerMove();
-				//if (game.checkWinner(game.getUserSign())) {
-				//	break;
-				//}
+			if (game.tie()) {
+				System.out.println("Game is Tie\n");
+				break;
 			}
 			game.changeTurn();
 		}
